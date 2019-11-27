c42e55077ee2cce1528538400d3e80cb83a2c004
Tzashinorpu <yzsnuaa@126.com>
2019-11-27 22:41:09 +0800

    disjointSet

--- src/Week06/Chapter13/LeetCode_6_130.java
+++ src/Week06/Chapter13/LeetCode_6_130.java
@@ -13,7 +13,7 @@ public class LeetCode_6_130 {
             count = 0;
             int m = grid.length;
             int n = grid[0].length;
-            parent = new int[m * n];
+            parent = new int[m * n + 1];
             for (int i = 0; i < m; ++i) {
                 for (int j = 0; j < n; ++j) {
                     if (grid[i][j] == 'O') {
@@ -22,6 +22,8 @@ public class LeetCode_6_130 {
                     }
                 }
             }
+            // dummyNode
+            parent[m * n] = m * n;
         }
 
 
@@ -33,11 +35,15 @@ public class LeetCode_6_130 {
             return p;
         }
 
+        public boolean isConnected(int p, int q) {
+            return find(p) == find(q);
+        }
+
         public void union(int p, int q) {
             int rootP = find(p);
             int rootQ = find(q);
             if (rootP == rootQ) return;
-            parent[rootQ] = rootP;
+            parent[rootP] = rootQ;
             count--;
         }
 
@@ -46,29 +52,39 @@ public class LeetCode_6_130 {
         }
     }
 
-    public UnionFind numIslands(char[][] M) {
+    public UnionFind solve(char[][] M) {
         if (M == null || M.length == 0) {
             return null;
         }
         int n = M.length;
         int m = M[0].length;
+        int dummyNode = n * m;
         UnionFind uf = new UnionFind(M);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int index1 = i * m + j;
                 if (M[i][j] == 'O') {
+                    int index1 = i * m + j;
+                    if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                        uf.union(index1, dummyNode);
+                    }
                     // 向右
-                    if ((j + 1) < m && M[i][j + 1] == 'O' ) {
+                    if ((j + 1) < m && M[i][j + 1] == 'O') {
                         // 方便debug
                         int index2 = i * m + j + 1;
                         uf.union(index1, index2);
                     }
                     // 向下
-                    if ((i + 1) < n && M[i + 1][j] == 'O' ) {
+                    if ((i + 1) < n && M[i + 1][j] == 'O') {
                         int index2 = (i + 1) * m + j;
                         uf.union(index1, index2);
                     }
-
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!uf.isConnected(i * m + j, dummyNode)) {
+                    M[i][j] = 'X';
                 }
             }
         }
@@ -78,13 +94,24 @@ public class LeetCode_6_130 {
 
     public static void main(String[] args) {
         char[][] area = {
-                {'O', 'X', 'X', 'X'},
-                {'X', 'O', 'O', 'X'},
-                {'X', 'O', 'X', 'X'},
-                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'O', 'X', 'X', 'X', 'O', 'X', 'O', 'O'},
+                {'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                {'X', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'X', 'X'},
+                {'X', 'O', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'O'},
+                {'O', 'X', 'X', 'X', 'O', 'X', 'O', 'X', 'O', 'X'},
+                {'X', 'X', 'O', 'X', 'X', 'O', 'O', 'X', 'X', 'X'},
+                {'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X', 'X', 'O'},
+                {'O', 'X', 'X', 'X', 'X', 'X', 'O', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'O'},
+                {'X', 'X', 'X', 'O', 'O', 'X', 'O', 'X', 'X', 'O'}
         };
         LeetCode_6_130 test = new LeetCode_6_130();
-        UnionFind unionFind = test.numIslands(area);
-        System.out.println("xxx");
+        UnionFind unionFind = test.solve(area);
+        for (int i = 0; i < area.length; i++) {
+            for (int j = 0; j < area[0].length; j++) {
+                System.out.printf("%c\t", area[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
