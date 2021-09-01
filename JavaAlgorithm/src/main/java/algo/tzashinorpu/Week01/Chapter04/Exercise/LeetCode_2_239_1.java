4d9594e02fd6bbc7d639e616fbc51cfb4f1931d3
TzashiNorpu <tzashinorpu@gmail.com>
2021-09-01 18:04:47 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/Week01/Chapter04/Exercise/LeetCode_2_239_1.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/Week01/Chapter04/Exercise/LeetCode_2_239_1.java
@@ -22,37 +22,18 @@ public class LeetCode_2_239_1 {
         int length = nums.length;
         int[] res = new int[length -k+1];
         Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0, j = k-1; i <=j; i++,j--) {
+        for (int i = 0; i < nums.length; i++) {
             if (stack.isEmpty()) {
                 stack.push(nums[i]);
             }else {
-                if (nums[i] > stack.peek()) {
-                    stack.addFirst(nums[i]);
-                }else {
-                    stack.addLast(nums[i]);
-                }
-                if (nums[j] > stack.peek()) {
-                    stack.addFirst(nums[j]);
-                }else {
-                    stack.addLast(nums[j]);
-                }
+
             }
         }
-        for (int i = k; i < nums.length; i++) {
-            // i = 7
-            res[i-k] = stack.peek();
-            if (nums[i] > stack.peek()) {
-                stack.addFirst(nums[i]);
-            }else {
-                stack.addLast(nums[i]);
-            }
-        }
-//        res[length - k + 1] = stack.peek();
         return res;
     }
 
     public static void main(String[] args) {
-        int[] ints = {1,3,-1,-3,5,3,6,7};
+        int[] ints = {1,3,2,-3,2,3,6,7};
         LeetCode_2_239_1 sol = new LeetCode_2_239_1();
         int[] res = sol.maxSlidingWindow2(ints, 3);
         for (int e : res) {
