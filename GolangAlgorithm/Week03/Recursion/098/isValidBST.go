0bf36d9313aa4c08b86a488e3220998224cd4ab7
TzashiNorpu <tzashinorpu@gmail.com>
2021-09-07 11:18:55 +0800

    202109071115

--- GolangAlgorithm/Week03/Recursion/098/isValidBST.go
+++ GolangAlgorithm/Week03/Recursion/098/isValidBST.go
@@ -1,7 +1,42 @@
 package isValidBST
 
-import "Week03/Tree/BinaryTree"
+import (
+	"Week03/Tree/BinaryTree"
+	"math"
+)
 
-func isValidBST(root *BinaryTree.TreeNode) bool {
-	return true
+func isValidBST1(root *BinaryTree.TreeNode) bool {
+	var inorderTraverse func(root *BinaryTree.TreeNode) bool
+	res := make([]int, 0)
+	inorderTraverse = func(root *BinaryTree.TreeNode) bool {
+		if root == nil {
+			return true
+		}
+		boolLeft := inorderTraverse(root.Left)
+		if len(res) > 0 {
+			if root.Val <= res[len(res)-1] {
+				return false
+			}
+		}
+		res = append(res, root.Val)
+		boolRight := inorderTraverse(root.Right)
+		return boolLeft && boolRight
+	}
+	return inorderTraverse(root)
+}
+
+func isValidBST2(root *BinaryTree.TreeNode) bool {
+	return helper(root, math.MinInt64, math.MaxInt64)
+}
+
+func helper(root *BinaryTree.TreeNode, lower int, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	// 左边分分支用当前节点值作为upper->左边节点值都应当小于upper
+	// 右边分分支用当前节点值作为lower->右边节点值都应当大于lower
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
 }
--- GolangAlgorithm/Week03/Recursion/098/isValidBST_test.go
+++ GolangAlgorithm/Week03/Recursion/098/isValidBST_test.go
@@ -1,7 +1,21 @@
 package isValidBST
 
-import "testing"
+import (
+	"Week03/Tree/BinaryTree"
+	"fmt"
+	"testing"
+)
 
 func TestIsValidBST(t *testing.T) {
+	/*root := &BinaryTree.TreeNode{Val: 5}
+	root.Left = &BinaryTree.TreeNode{Val: 4}
+	root.Right = &BinaryTree.TreeNode{Val: 6}
+	root.Right.Left = &BinaryTree.TreeNode{Val: 3}
+	root.Right.Right = &BinaryTree.TreeNode{Val: 7}*/
 
+	root := &BinaryTree.TreeNode{Val: 2}
+	root.Left = &BinaryTree.TreeNode{Val: 1}
+	root.Right = &BinaryTree.TreeNode{Val: 6}
+	fmt.Println(isValidBST1(root))
+	fmt.Println(isValidBST2(root))
 }
