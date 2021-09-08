bbd82e92cb811e3847c8f97a6d013c72e6806d3d
Tzashinorpu <yzsnuaa@126.com>
2021-09-08 17:33:23 +0800

    commit

--- GolangAlgorithm/Week03/Recursion/236/lowestCommonAncestor.go
+++ GolangAlgorithm/Week03/Recursion/236/lowestCommonAncestor.go
@@ -3,29 +3,19 @@ package lowestCommonAncestor
 import "Week03/Tree/BinaryTree"
 
 func lowestCommonAncestor(root, p, q *BinaryTree.TreeNode) *BinaryTree.TreeNode {
-	mapCount := map[int]int{}
-	pVal := p.Val
-	qVal := q.Val
-	var res *BinaryTree.TreeNode
-	var dfs func(root *BinaryTree.TreeNode, val int)
-	dfs = func(root *BinaryTree.TreeNode, val int) {
-		if root == nil {
-			return
-		}
-		mapCount[root.Val] = mapCount[root.Val] + 1
-		if root.Left.Val == val || root.Right.Val == val {
-			return
-		}
-		dfs(root.Left, val)
-		dfs(root.Right, val)
+	if root == nil || root == p || root == q {
+		return root
 	}
-	dfs(root, pVal)
-	dfs(root, qVal)
-	for k, v := range mapCount {
-		if v == 2 {
-			res = &BinaryTree.TreeNode{Val: k}
-			break
-		}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left == nil && right == nil {
+		return nil
 	}
-	return res
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
+	return root
 }
--- GolangAlgorithm/Week03/Recursion/236/lowestCommonAncestor_test.go
+++ GolangAlgorithm/Week03/Recursion/236/lowestCommonAncestor_test.go
@@ -2,21 +2,24 @@ package lowestCommonAncestor
 
 import (
 	"Week03/Tree/BinaryTree"
+	"fmt"
 	"testing"
 )
 
 func TestLowestCommonAncestor(t *testing.T) {
 	root := &BinaryTree.TreeNode{Val: 3}
 	root.Left = &BinaryTree.TreeNode{Val: 5}
+	p := root.Left
 	root.Left.Left = &BinaryTree.TreeNode{Val: 6}
-	root.Left.Right = &BinaryTree.TreeNode{Val: 2}
+
+	q := root.Left.Left
+	/*root.Left.Right = &BinaryTree.TreeNode{Val: 2}
 	root.Left.Right.Left = &BinaryTree.TreeNode{Val: 7}
 	root.Left.Right.Right = &BinaryTree.TreeNode{Val: 4}
+	q := root.Left.Right.Right
 
 	root.Right = &BinaryTree.TreeNode{Val: 1}
 	root.Right.Left = &BinaryTree.TreeNode{Val: 0}
-	root.Right.Right = &BinaryTree.TreeNode{Val: 8}
-	p := &BinaryTree.TreeNode{Val: 5}
-	q := &BinaryTree.TreeNode{Val: 4}
-	lowestCommonAncestor(root, p, q)
+	root.Right.Right = &BinaryTree.TreeNode{Val: 8}*/
+	fmt.Println(lowestCommonAncestor(root, p, q).Val)
 }
