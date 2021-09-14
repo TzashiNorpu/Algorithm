3d5496450454a81a97a98e371cd72579510e825b
TzashiNorpu <yzsnuaa@126.com>
2021-09-14 23:07:13 +0800

    202109142300

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/Week02/Chapter07/LeetCode_2_105.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/Week02/Chapter07/LeetCode_2_105.java
@@ -2,11 +2,12 @@ package algo.tzashinorpu.Week02.Chapter07;
 
 import algo.tzashinorpu.Week02.Chapter06.BinaryTree;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LeetCode_2_105 {
-    public BinaryTree buildTree(int[] preorder, int[] inorder) {
+    public BinaryTree buildTree1(int[] preorder, int[] inorder) {
         //        preorder 根 左 右          inorder   左   根   右
         // preorder: 根节点|----------------左子树-----------------|-------------------右子树-------------------|
         //         preLeft|preLeft+1       preLeft+pIndex-inLeft|preLeft+pIndex-inLeft+1            preRight|
@@ -26,18 +27,45 @@ public class LeetCode_2_105 {
         int preRight = preLen - 1;
         int inLeft = 0;
         int inRight = inLen - 1;
-        return build(preorder, preLeft, preRight, inorder, inLeft, inRight, inorderMap);
+        return build1(preorder, preLeft, preRight, inorder, inLeft, inRight, inorderMap);
     }
 
-    private BinaryTree build(int[] preorder, int preLeft, int preRight, int[] inorder, int inLeft, int inRight, Map<Integer, Integer> inorderMap) {
+    private BinaryTree build1(int[] preorder, int preLeft, int preRight, int[] inorder, int inLeft, int inRight, Map<Integer, Integer> inorderMap) {
         if (preLeft > preRight || inLeft > inRight) {
             return null;
         }
         int rootVal = preorder[preLeft];
         BinaryTree root = new BinaryTree(rootVal);
         Integer pIndex = inorderMap.get(rootVal);
-        root.left = build(preorder, preLeft + 1, preLeft + pIndex - inLeft, inorder, inLeft, pIndex - 1, inorderMap);
-        root.right = build(preorder, preLeft + pIndex - inLeft + 1, preRight, inorder, pIndex + 1, inRight, inorderMap);
+        root.left = build1(preorder, preLeft + 1, preLeft + pIndex - inLeft, inorder, inLeft, pIndex - 1, inorderMap);
+        root.right = build1(preorder, preLeft + pIndex - inLeft + 1, preRight, inorder, pIndex + 1, inRight, inorderMap);
+        return root;
+    }
+
+
+    public BinaryTree buildTree2(int[] preorder, int[] inorder) {
+        int length = preorder.length;
+        if (length == 0) {
+            return null;
+        }
+        int rootVal=preorder[0];
+        BinaryTree root = new BinaryTree(rootVal);
+        if (length == 1) {
+            return root;
+        }
+        int splitIndex = -1;
+        for (int i = 0; i < length; i++) {
+            if (inorder[i]== rootVal) {
+                splitIndex=i;
+                break;
+            }
+        }
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, splitIndex+1);
+        int[] rightPreorder = Arrays.copyOfRange(preorder, splitIndex+1, length);
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, splitIndex);
+        int[] rightInorder = Arrays.copyOfRange(inorder, splitIndex+1, length);
+        root.left = buildTree2(leftPreorder, leftInorder);
+        root.right = buildTree2(rightPreorder, rightInorder);
         return root;
     }
 }
--- JavaAlgorithm/src/test/java/algo/tzashinorpu/Week02/Chapter07/LeetCode_2_105Test.java
+++ JavaAlgorithm/src/test/java/algo/tzashinorpu/Week02/Chapter07/LeetCode_2_105Test.java
@@ -1,10 +1,35 @@
 package algo.tzashinorpu.Week02.Chapter07;
 
+import algo.tzashinorpu.Week02.Chapter06.BinaryTree;
+import algo.tzashinorpu.Week02.Chapter06.LeetCode_2_094_1;
 import org.junit.jupiter.api.Test;
 
 class LeetCode_2_105Test {
 
+
     @Test
-    void buildTree() {
+    void buildTree1() {
+    }
+
+    @Test
+    void buildTree2() {
+        /*BinaryTree root = new BinaryTree(1);
+        root.left = new BinaryTree(2);
+        root.left.left = new BinaryTree(4);
+        root.left.right = new BinaryTree(5);
+        root.left.right.left = new BinaryTree(8);
+        root.left.right.right = new BinaryTree(9);
+
+        root.right = new BinaryTree(3);
+        root.right.left = new BinaryTree(6);
+        root.right.left.right = new BinaryTree(10);
+        root.right.right = new BinaryTree(7);*/
+        LeetCode_2_105 instance = new LeetCode_2_105();
+        int[] preorder = {1, 2, 4, 5, 8, 9, 3, 6, 10, 7};
+        int[] inorder = {4, 2, 8, 5, 9, 1, 6, 10, 3, 7};
+        BinaryTree root = instance.buildTree2(preorder, inorder);
+
+        LeetCode_2_094_1 traverse = new LeetCode_2_094_1();
+        System.out.println(traverse.inorderTraversal(root));
     }
 }
