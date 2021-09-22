b03578d65891d0092c73217d2535c6e070411d45
Tzashinorpu <yzsnuaa@126.com>
2021-09-22 19:57:14 +0800

    commit

--- JavascriptAlgorithm/Array/twoSum-001.js
+++ JavascriptAlgorithm/Array/twoSum-001.js
@@ -1,4 +1,18 @@
 var twoSum = function(nums, target) {
     let map=new Map();
-    
+    let res = [];
+    for (let index = 0; index < nums.length; index++) {
+        if(map[target-nums[index]]!=null){
+            res[0]=map[target-nums[index]];
+            res[1]=index;
+        }else{
+            map[nums[index]] = index;     
+        }
+    }
+    return res;    
 };
+
+let nums=[1,3,2,7];
+let target = 9;
+let res = twoSum(nums,target);
+console.log(res);
