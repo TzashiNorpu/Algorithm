9104c0d32a91129797ff84d995599db85c959d47
TzashiNorpu <TzashiNorpu@gmail.com>
2021-09-24 18:23:26 +0800

    'commit'

--- JavascriptAlgorithm/Array/moveZeros-283.js
+++ JavascriptAlgorithm/Array/moveZeros-283.js
@@ -2,6 +2,10 @@ var moveZeroes = function (nums) {
   for (let i = 0, j = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
       nums[j++] = nums[i];
+      // j - 1 恢复 j 的值
+      if (i !== j - 1) {
+        nums[i] = 0;
+      }
     }
   }
   return nums;
