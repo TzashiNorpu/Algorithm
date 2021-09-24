6a05d883cf530f82a8d18673a6a02bee27f14730
TzashiNorpu <tzashinorpu@gmail.com>
2021-09-24 19:02:43 +0800

    202109241900

--- JavascriptAlgorithm/LinkedList/mergeTwoLists-021.js
+++ JavascriptAlgorithm/LinkedList/mergeTwoLists-021.js
@@ -1,9 +1,9 @@
-import { ListNode } from "./ListNode.js"
+import { ListNode } from "./ListNode.js";
 var mergeTwoLists = function (l1, l2) {
-    let dummyHead = new ListNode(0);
-    let concat = dummyHead;
-    // 1 2 5 + 1  4
-    while (l1 !== null || l2 !== null) {
+  let dummyHead = new ListNode(0);
+  let concat = dummyHead;
+  // 1 2 4 + 1 3 4
+  /* while (l1 !== null || l2 !== null) {
         if (l1 === null) {
             concat.next = l2;
             return dummyHead.next;
@@ -19,7 +19,21 @@ var mergeTwoLists = function (l1, l2) {
             concat.next = l2;
             l2 = l2.next;
         }
+        concat = concat.next;
+    } */
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      concat.next = l1;
+      l1 = l1.next;
+    } else {
+      concat.next = l2;
+      l2 = l2.next;
     }
-    return dummyHead.next;
+    concat = concat.next;
+  }
+//   concat.next = l1 === null ? l2 : l1;
+  concat.next = l1 || l2;
+
+  return dummyHead.next;
 };
 export { mergeTwoLists };
