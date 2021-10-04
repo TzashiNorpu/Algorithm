ed7ad8179fb489a2316daeea5d080f9e068da962
TzashiNorpu <yzsnuaa@126.com>
2021-10-04 22:17:16 +0800

    commit

--- JavascriptAlgorithm/Array/maxSlidingWindow-239.js
+++ JavascriptAlgorithm/Array/maxSlidingWindow-239.js
@@ -14,22 +14,19 @@ var maxSlidingWindow2 = function (nums, k) {
   let res = [],
     stack = [];
   // 维护单调递减队列
-  let maxIndex = 0;
   for (let i = 0; i < nums.length; i++) {
-    while (stack.length > 0 && nums[i] > nums[stack[stack.length - 1]]) {
+    // = 影响性能：弹出尽可能多的元素，后续的shift就会提高性能
+    while (stack.length > 0 && nums[i] >= nums[stack[stack.length - 1]]) {
       stack.pop();
     }
     stack.push(i);
+    
+    if (i - stack[0] >= k) {
+        stack.shift();
+    }
     if (i >= k - 1) {
-      //   res.push(nums[stack[0]]);
-      res.push(nums[stack[maxIndex]]);
-    }
-    // if (i - stack[0] >= k) {
-    if (i - stack[maxIndex] + 1 >= k) {
-      //   stack.shift();
-      maxIndex++;
-      // shift 影响性能 用 maxIndex 变量代替
-    }
+      res.push(nums[stack[0]]);
+  }
   }
   return res;
 };
