268f3700058509c34c5805e5f03031f564902ce4
TzashiNorpu <tzashinorpu@gmail.com>
2021-10-20 14:26:21 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_127.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_127.java
@@ -7,31 +7,59 @@ import java.util.List;
 
 public class LeetCode_3_127 {
     // BFS
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
         LinkedList<String> stack = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+        HashSet<String> set = new HashSet<>(wordList);
         int depth = 0;
         stack.offer(beginWord);
+        set.remove(beginWord);
+        visited.add(beginWord);
         while (stack.size() > 0) {
             depth++;
             int size = stack.size();
             for (int i = 0; i < size; i++) {
+                String poll = stack.poll();
+                char[] chars = poll.toCharArray();
+                for (int j = 0; j < chars.length; j++) {
+                    char oldChar = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[j] = c;
+                        String oneLetterChangStr = new String(chars);
+                        if (set.contains(oneLetterChangStr)) {
+                            if (oneLetterChangStr.equals(endWord)) {
+                                return depth + 1;
+                            } else {
+                                stack.offer(oneLetterChangStr);
+                                set.remove(oneLetterChangStr);
+                            }
+                        }
+                    }
+                    chars[j] = oldChar;
+                }
+            }
+            /*for (int i = 0; i < size; i++) {
                 String poll = stack.poll();
                 for (int j = 0; j < wordList.size(); j++) {
                     String item = wordList.get(j);
                     if (poll.equals(endWord)) {
                         return depth;
                     }
+                    if (visited.contains(item)) {
+                        continue;
+                    }
                     if (isOneLetterChange(item, poll)) {
                         stack.offer(item);
+                        visited.add(item);
                     }
                 }
-            }
+            }*/
         }
         return 0;
     }
 
     private boolean isOneLetterChange(String s1, String s2) {
-        int cnt=0;
+        int cnt = 0;
         if (s1.length() != s2.length()) {
             return false;
         }
@@ -40,6 +68,64 @@ public class LeetCode_3_127 {
                 cnt++;
             }
         }
-        return cnt==1;
+        return cnt == 1;
+    }
+
+    // double bfs：一边从 start 依次替换单个字符，一边从 end 依次替换单个字符，在中间碰头时即为答案
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        if (wordList == null || wordList.size() == 0) return 0;
+        //hashset的好处：去重也完成了
+        //开始端
+        HashSet<String> start = new HashSet<>();
+        //结束端
+        HashSet<String> end = new HashSet<>();
+        //所有字符串的字典
+        HashSet<String> dic = new HashSet<>(wordList);
+        start.add(beginWord);
+        end.add(endWord);
+        if (!dic.contains(endWord)) return 0;
+        //经历过上面的一系列判定，到这里的时候，若是有路径，则最小是2，所以以2开始
+        return bfs(start, end, dic, 2);
+    }
+
+    private int bfs(HashSet<String> st, HashSet<String> ed, HashSet<String> dic, int l) {
+        //双端查找的时候，若是有任意一段出现了“断裂”，也就是说明不存在能够连上的路径，则直接返回0
+        if (st.size() == 0) return 0;
+        if (st.size() > ed.size()) {//双端查找，为了优化时间，永远用少的去找多的，比如开始的时候塞进了1000个，而结尾只有3个，则肯定是从少的那一端开始走比较好
+            return bfs(ed, st, dic, l);
+        }
+        //BFS的标记行为，即使用过的不重复使用
+        dic.removeAll(st);
+        //收集下一层临近点
+        HashSet<String> next = new HashSet<>();
+        for (String s : st) {
+            /*for (String ite : dic) {
+                if (isOneLetterChange(s, ite)) {
+                    if (ed.contains(ite)) {
+                        return l;
+                    } else {
+                        next.add(ite);
+                    }
+                }
+            }*/
+            // 下面这种更快
+            char[] arr = s.toCharArray();
+            for (int i = 0; i < arr.length; i++) {
+                char tmp = arr[i];
+                //变化
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (tmp == c) continue;
+                    arr[i] = c;
+                    String nstr = new String(arr);
+                    if (dic.contains(nstr)) {
+                        if (ed.contains(nstr)) return l;
+                        else next.add(nstr);
+                    }
+                }
+                //复原
+                arr[i] = tmp;
+            }
+        }
+        return bfs(next, ed, dic, l + 1);
     }
 }
--- JavaAlgorithm/src/test/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_127Test.java
+++ JavaAlgorithm/src/test/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_127Test.java
@@ -4,20 +4,31 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LeetCode_3_127Test {
 
     @org.junit.jupiter.api.Test
     void ladderLength() {
-       // beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
-        String beginword="hit";
-        String endword = "cog";
-
-        String[] strings = {"hot", "dot", "dog", "lot", "log", "cog"};
-        List<String> wordlist = new LinkedList<>(Arrays.asList(strings));
         LeetCode_3_127 instance = new LeetCode_3_127();
-        int i = instance.ladderLength(beginword, endword, wordlist);
-        System.out.println(i);
+
+/*        String beginword1 = "hit";
+        String endword1 = "cog";
+        String[] strings1 = {"hot", "dot", "dog", "lot", "log", "cog"};
+        List<String> wordlist1 = new LinkedList<>(Arrays.asList(strings1));
+        System.out.printf("ladderLength1 result=%d\n", instance.ladderLength1(beginword1, endword1, wordlist1));
+        System.out.printf("ladderLength2 result=%d\n", instance.ladderLength2(beginword1, endword1, wordlist1));
+
+        String beginword2 = "hit";
+        String endword2 = "cog";
+        String[] strings2 = {"hot", "dot", "dog", "lot", "log"};
+        List<String> wordlist2 = new LinkedList<>(Arrays.asList(strings2));
+        System.out.printf("ladderLength1 result=%d\n", instance.ladderLength1(beginword2, endword2, wordlist2));
+        System.out.printf("ladderLength2 result=%d\n", instance.ladderLength2(beginword2, endword2, wordlist2));*/
+
+        String beginword3 = "ymain";
+        String endword3 = "oecij";
+        String[] strings3 = {"ymann", "yycrj", "oecij", "ymcnj", "yzcrj", "yycij", "xecij", "yecij", "ymanj", "yzcnj", "ymain"};
+        List<String> wordlist3 = new LinkedList<>(Arrays.asList(strings3));
+        System.out.printf("ladderLength1 result=%d\n", instance.ladderLength1(beginword3, endword3, wordlist3));
+        System.out.printf("ladderLength2 result=%d\n", instance.ladderLength2(beginword3, endword3, wordlist3));
     }
 }
