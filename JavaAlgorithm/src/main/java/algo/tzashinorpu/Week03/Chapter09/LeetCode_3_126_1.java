6802762cf205643c97e4b70c2326286f6a50335b
tzashinorpu <tzashinorpu@google.com>
2021-10-24 15:01:36 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_126_1.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/Week03/Chapter09/LeetCode_3_126_1.java
@@ -1,19 +1,17 @@
 package algo.tzashinorpu.Week03.Chapter09;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LeetCode_3_126_1 {
     public List<List<String>> res = new LinkedList<>();
 
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
         HashSet<Character> letters = new HashSet<>();
-        wordList.forEach(word -> {
+        /*wordList.forEach(word -> {
             for (char c : word.toCharArray()) {
                 letters.add(c);
             }
-        });
+        });*/
 //        System.out.println(letters);
         HashSet<String> dic = new HashSet<>(wordList);
         if (!dic.contains(endWord)) {
@@ -39,7 +37,8 @@ public class LeetCode_3_126_1 {
         char[] chars = beginWord.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             char oldChar = chars[i];
-            for (char c : letters) {
+//            for (char c : letters) {
+                for (char c = 'a';  c<='z'; c++) {
                 chars[i] = c;
                 String s = new String(chars);
                 if (dic.contains(s)) {
@@ -98,4 +97,74 @@ public class LeetCode_3_126_1 {
         return 0;
     }
 
+    public List<List<String>> findLadders2(String beginWord, String endWord, List<String> wordList) {
+        // 记录单词出现的层数
+        HashMap<String, Integer> steps = new HashMap<>();
+        //　记录该单词可以从哪些单词变换而来
+        HashMap<String, LinkedList<String>> from = new HashMap<>();
+        List<List<String>> res = new LinkedList<>();
+
+        HashSet<String> dic = new HashSet<>(wordList);
+        if (!dic.contains(endWord)) {
+            return res;
+        }
+        boolean levelFound =false;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        dic.remove(beginWord);
+        steps.put(beginWord, 0);
+        int step=1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String fromWord = queue.poll();
+                char[] chars = fromWord.toCharArray();
+                for (int j = 0; j < chars.length; j++) {
+                    char oldChar = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[j]=c;
+                        String toWord = new String(chars);
+                        // toWord 是当前层的变换　且　toWord 已经在当前层上一次变换时从　dic 中删除　时更新
+                         if (steps.containsKey(toWord) && step == steps.get(toWord)) {
+                            from.get(toWord).add(fromWord);
+                        }
+                        if (!dic.contains(toWord)) {
+                            continue;
+                        }
+                        dic.remove(toWord);
+                        queue.offer(toWord);
+                        from.putIfAbsent(toWord, new LinkedList<>());
+                        from.get(toWord).add(fromWord);
+                        steps.put(toWord, step);
+                        if (toWord.equals(endWord)) {
+                                levelFound = true;
+                        }
+                    }
+                    chars[j]=oldChar;
+                }
+            }
+            step++;
+            if (levelFound) {
+                break;
+            }
+        }
+        if (levelFound) {
+            ArrayDeque<String> path = new ArrayDeque<>();
+            path.add(endWord);
+            dfs(from, path, beginWord, endWord, res);
+        }
+        return res;
+    }
+
+    private void dfs(HashMap<String, LinkedList<String>> from, ArrayDeque<String> path, String beginWord, String curr, List<List<String>> res) {
+        if (curr.equals(beginWord)) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (String s : from.get(curr)) {
+            path.addFirst(s);
+            dfs(from, path, beginWord, s, res);
+            path.remove(s);
+        }
+    }
 }
