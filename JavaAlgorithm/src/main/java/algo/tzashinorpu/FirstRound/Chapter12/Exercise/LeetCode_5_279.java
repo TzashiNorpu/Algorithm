bf1cac3bc64cd65c51c65186d9a1b8470a311c2e
TzashiNorpu <tzashinorpu@gmail.com>
2021-11-23 14:49:15 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/FirstRound/Chapter12/Exercise/LeetCode_5_279.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/FirstRound/Chapter12/Exercise/LeetCode_5_279.java
@@ -2,6 +2,16 @@ package algo.tzashinorpu.FirstRound.Chapter12.Exercise;
 
 public class LeetCode_5_279 {
     public int numSquares(int n) {
-
+        // 13
+        // [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+        // [0,1,2,3,1,2,3,4,2,1, 2, 3, 3, 2]
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = i;
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 }
---
+++ JavaAlgorithm/src/test/java/algo/tzashinorpu/FirstRound/Chapter12/Exercise/LeetCode_5_279Test.java
@@ -0,0 +1,14 @@
+package algo.tzashinorpu.FirstRound.Chapter12.Exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode_5_279Test {
+
+    @Test
+    void rob() {
+        LeetCode_5_279 ins = new LeetCode_5_279();
+        System.out.println(ins.numSquares(13));
+    }
+}
