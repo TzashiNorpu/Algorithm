4d26ceaf3e96bd1c6a2b7d44f75d8d3be64e2011
TzashiNorpu <tzashinorpu@gmail.com>
2021-12-31 18:02:57 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/SecondRound/Chapter12/maxProfit_123.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/SecondRound/Chapter12/maxProfit_123.java
@@ -3,12 +3,23 @@ package algo.tzashinorpu.SecondRound.Chapter12;
 public class maxProfit_123 {
     public int maxProfit(int[] prices) {
         // 最多两笔交易
+        // 0:买入 1:完成一笔交易  2:一笔交易+买入  3:两笔交易
         int length = prices.length;
-        int[][] dp = new int[length][2];
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < 2; j++) {
-                
-            }
+        int[][] dp = new int[length][4];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[0][2] = -prices[0];
+        dp[0][3] = 0;
+        int max = dp[0][3];
+        for (int i = 1; i < length; i++) {
+//            for (int j = 0; j < 4; j++) {
+            dp[i][0] = Math.max(-prices[i], dp[i - 1][0]);
+            dp[i][1] = prices[i] + dp[i][0];
+            dp[i][2] = dp[i][1] - prices[i];
+            dp[i][3] = dp[i][2] + prices[i];
+            max = Math.max(dp[i][3], max);
+//            }
         }
+        return max;
     }
 }
