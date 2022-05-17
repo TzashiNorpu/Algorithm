7d4d29a8845257843e045acbb3903cc8a9836bcf
TzashiNorpu <TzashiNorpu@gmail.com>
2022-05-17 20:07:36 +0800

    commit

---
+++ JavaAlgorithm/src/test/java/algo/tzashinorpu/SpecialSubject/Top/countAndSay_38Test.java
@@ -0,0 +1,17 @@
+package algo.tzashinorpu.SpecialSubject.Top;
+
+import org.junit.jupiter.api.Test;
+
+class countAndSay_38Test {
+
+    @Test
+    void countAndSay() {
+        countAndSay_38 ins = new countAndSay_38();
+        System.out.println(ins.countAndSay(1));
+        System.out.println(ins.countAndSay(2));
+        System.out.println(ins.countAndSay(3));
+        System.out.println(ins.countAndSay(4));
+        System.out.println(ins.countAndSay(5));
+        System.out.println(ins.countAndSay(6));
+    }
+}
