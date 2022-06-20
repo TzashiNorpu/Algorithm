4aac0eb0acb26eaf50a07490c20f6da25f3333ed
TzashiNorpu <yzsnuaa@126.com>
2022-06-20 22:48:25 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/SpecialSubject/Stack_Queue/constrainedSubsetSum_1425.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/SpecialSubject/Stack_Queue/constrainedSubsetSum_1425.java
@@ -4,33 +4,34 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class constrainedSubsetSum_1425 {
+    /*
+    输入：nums = [10,-2,-7,-5,20], k = 2
+    输出：23
+    解释：子序列为 [10, -2, -5, 20] 。
+     */
     public int constrainedSubsetSum(int[] nums, int k) {
-        /*
-        Input: nums = [10,2,-10,5,20], k = 2
-        Output: 37
-        Explanation: The subsequence is [10, 2, 5, 20]
-         */
-        // A[i] = max(0, A[i - k], A[i - k + 1],..,A[i - 1])+A[i] -> dp[i] = max(0, dp[i - k], dp[i - k + 1], .., dp[i -1]) + A[i]
-        // `deque` stores dp[i - k], dp[i-k+1], .., dp[i - 1] whose values are larger than 0 in a decreasing order
-        int res = nums[0];
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; ++i) {
-            // deque[0] is the max of (0, dp[i - k], dp[i-k+1], .., dp[i - 1])
-            nums[i] += !q.isEmpty() ? q.peek() : 0;
-            res = Math.max(res, nums[i]);
-            // We always want to retrieve the max of (0, dp[i - k], dp[i-k+1], .., dp[i - 1]) from `deque`
-            // We expect dp[i] to be added to `deque` so that we can compute dp[i + 1] in the next iteration
-            // So, if dp[i] is larger than some old values, we can discard them safely.
-            // As a result, the length of `deque` is not necessarily `k`
-            while (!q.isEmpty() && nums[i] > q.peekLast())
-                q.pollLast();
-            // no need to store the negative value
-            if (nums[i] > 0)
-                q.offer(nums[i]);
-            // we do not need the value of A[i - k] when computing dp[i+1] in the next iteration, because `j - i <= k` has to be satisfied.
-            if (i >= k && !q.isEmpty() && q.peek() == nums[i - k])
-                q.poll();
+        int n = nums.length;
+        int[] f = new int[n];
+        f[0] = nums[0];
+        Deque<Integer> q = new ArrayDeque<Integer>();
+        q.addLast(0);
+        int ans = nums[0];
+        for (int i = 1; i < n; ++i) {
+            // 如果队首的 j 与 i 的差值大于 k，则不满足要求，弹出
+            while (!q.isEmpty() && i - q.peekFirst() > k) {
+                q.removeFirst();
+            }
+            // 此时队首的 j 即为最优的 j 值
+            // 状态转移
+            f[i] = Math.max(f[q.peekFirst()], 0) + nums[i];
+            ans = Math.max(ans, f[i]);
+            // 维护队列的单调性，不断从队尾弹出元素
+            while (!q.isEmpty() && f[i] >= f[q.peekLast()]) {
+                q.removeLast();
+            }
+            // 将 i 作为之后的新 j 值放入队尾
+            q.addLast(i);
         }
-        return res;
+        return ans;
     }
 }
