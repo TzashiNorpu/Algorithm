9bbae575786e33027f7c2c4823ba8ceda2e1a51f
TzashiNorpu <TzashiNorpu@gmail.com>
2022-06-27 15:50:18 +0800

    commit

--- Cpp/Algorithm/src/main.cpp
+++ Cpp/Algorithm/src/main.cpp
@@ -1,13 +1,15 @@
 #include "SpecialSubject/ss.h"
 #include "GSSY/gssy.h"
+#include "SpecialSubject/Graph/graph.h"
 using namespace myAlgo;
 using namespace GSSY;
 #include<iostream>
 #include <map>
 int main()
 {
-	LargestRectangleArea_84 ins;
-	vector<int> h = { 2,1,5,6,2,3 };
-	int res=ins.largestRectangleArea(h);
-	cout << res << endl;
+	MinimumScore_2322 ins;
+	vector<int> nums = {1,5,5,4,11};
+	vector<vector<int>> edges = { {0,1},{1,2},{1,3},{3,4} };
+	int res=ins.minimumScore(nums,edges);
+	printf("res=%d",res);
 }
