11cdcb25f546a474a779b580865b25b6d62f404b
TzashiNorpu <TzashiNorpu@gmail.com>
2022-07-03 17:16:03 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/WeekContest/D220703/peopleAwareOfSecret_6109.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/WeekContest/D220703/peopleAwareOfSecret_6109.java
@@ -2,17 +2,21 @@ package algo.tzashinorpu.WeekContest.D220703;
 
 public class peopleAwareOfSecret_6109 {
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        int[][] dp = new int[n][3];
+        int[][] dp = new int[n][forget];
+        int sum = 0;
         dp[0][0] = 1;
-        dp[0][1] = forget == 0 ? dp[0][0] : 0;
-        dp[0][2] = delay == 0 ? dp[0][0] : 0;
+        int MOD = 1000000007;
         for (int i = 1; i < n; i++) {
-            // forget
-            dp[i][1] = i - forget >= 0 ? dp[i - forget][0] : 0;
-            // can't share
-            dp[i][2] = dp[i - 1][0] - dp[i - 1][1];
-            dp[i][0] = (dp[i - 1][0] - dp[i][1] - dp[i][2]) * 2 + dp[i][2];
+            for (int j = delay - 1; j < forget - 1; j++) {
+                dp[i][0] = (dp[i][0] + dp[i - 1][j]) % MOD;
+            }
+            for (int j = 1; j < forget; j++) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+            }
         }
-        return dp[n - 1][0];
+        for (int j = 0; j < forget; j++) {
+            sum = (sum + dp[n - 1][j]) % MOD;
+        }
+        return sum;
     }
 }
