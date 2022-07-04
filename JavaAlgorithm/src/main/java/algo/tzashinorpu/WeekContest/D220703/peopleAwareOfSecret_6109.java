2d3b8b5f9746ac391858ab3b3819a4f8668c3d4e
TzashiNorpu <TzashiNorpu@gmail.com>
2022-07-04 10:32:03 +0800

    commit

--- JavaAlgorithm/src/main/java/algo/tzashinorpu/WeekContest/D220703/peopleAwareOfSecret_6109.java
+++ JavaAlgorithm/src/main/java/algo/tzashinorpu/WeekContest/D220703/peopleAwareOfSecret_6109.java
@@ -3,27 +3,26 @@ package algo.tzashinorpu.WeekContest.D220703;
 public class peopleAwareOfSecret_6109 {
     public int peopleAwareOfSecret(int n, int delay, int forget) {
 
-        // dp[i]的含义是第i天获取秘密的人
-        /*
-        统计第i天新增的人数就好了，然后知道秘密的总人数其实就等于从最后一天往前推forget天的人数和。
-        每一个第i天知道秘密的人，都对[i+delay,i+forget)这个区间有贡献
-         */
         int[] dp = new int[n + 1];
         dp[1] = 1;
         int MOD = 1000000007;
         for (int i = 1; i < n + 1; i++) {
             /*for (int j = i + delay; j < i + forget; j++) {
-                if (j <= n) dp[j] += dp[i];
+                if (j <= n) (dp[j] + dp[i]) % MOD;
             }*/
             for (int j = i + delay; j < Math.min(i + forget, n + 1); j++) {
-                dp[j] += dp[i];
+                dp[j] = (dp[j] + dp[i]) % MOD;
             }
+            // [i-forget+1,i-delay]
+            // i=2->[-1,0]    i=3->[0,1]   i=5->[2,3]
+            /*for (int j = i - forget + 1; j <= i - delay; j++) {
+                if (j >= 0 && j <= n) dp[i] = (dp[i] + dp[j]) % MOD;
+            }*/
         }
         int sum = 0;
         for (int i = n + 1 - forget; i < n + 1; i++) {
             sum = (sum + dp[i]) % MOD;
         }
-
         return sum;
     }
 }
